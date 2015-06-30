aa11ca58ef0dd2ae3494fcfadfef3d302039a530
unknown <tlushney@NASS.softservecom.com>
2015-06-30 20:10:36 +0300

    Fixed singleton class.

--- singleton.h
+++ singleton.h
@@ -1,22 +1,31 @@
 #ifndef SINGLETON_H_
 #define SINGLETON_H_
- 
+
+#include <mutex>
+#include <memory>
+#include <utility>
+
 template <class T>
 class Singleton {
  public:
     template <typename... Args>
     static T& instance(Args&&... args) {
-        call_once(once_flag(), [&, args...]() {
+        std::call_once(once_flag(), [](Args&&... args) {
             instance_.reset(new T(std::forward<Args>(args)...));
-        });
+        }, std::forward<Args>(args)...);
       return *instance_.get();
     }
  
  protected:
-    explicit Singleton<T>() = default;
+	 explicit Singleton<T>() = default;
+
     ~Singleton<T>() = default;
- 
+
  private:
+	Singleton<T>(const Singleton<T>&) = delete;
+	
+	const Singleton<T>& operator=(const Singleton<T>&) = delete;
+ 
     static std::once_flag& once_flag() {
         static std::once_flag once;
         return once;
