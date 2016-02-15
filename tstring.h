cbb5084ddee9395fc4761b2d6af44d397303e7af
unknown <tlushney@NASS.softservecom.com>
2016-02-15 16:37:09 +0200

    Minor changes

---
+++ tstring.h
@@ -0,0 +1,16 @@
+//  Created by Taras Lushney on 10/02/16.
+//  Copyright (c) 2015 Taras Lushney. All rights reserved.
+
+#ifndef TSTRING_H_
+#define TSTRING_H_
+
+#include <windows.h>
+#include <string>
+
+namespace win32_api {
+
+typedef std::basic_string<TCHAR> TString;
+
+}  // namespace win32_api
+
+#endif  // TSTRING_H_
